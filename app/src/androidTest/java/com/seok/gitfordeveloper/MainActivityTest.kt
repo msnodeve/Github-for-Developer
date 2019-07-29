47c49062a2a3263a025531f61265cde57cb16f6d
msnodeve <msnodeve@gmail.com>
2019-07-29 09:48:08 +0900

    LauncherActivity 로딩 구현

--- app/src/androidTest/java/com/seok/gitfordeveloper/MainActivityTest.kt
+++ app/src/androidTest/java/com/seok/gitfordeveloper/MainActivityTest.kt
@@ -4,15 +4,23 @@ import android.support.test.runner.AndroidJUnit4
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.filters.LargeTest
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MainActivityTest {
+
     @Rule
     @JvmField
     var activityRule = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun onCreate() {
+        val mainActivity = MainActivity()
+        mainActivity.onCreate(null, null)
+    }
 
 
 }
--- app/src/main/java/com/seok/gitfordeveloper/LauncherActivity.kt
+++ app/src/main/java/com/seok/gitfordeveloper/LauncherActivity.kt
@@ -1,5 +1,6 @@
 package com.seok.gitfordeveloper
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -10,8 +11,10 @@ class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))
+        }, 2000)
+        finish()
     }
-
-    // App Logo 2초 구현
-
 }
