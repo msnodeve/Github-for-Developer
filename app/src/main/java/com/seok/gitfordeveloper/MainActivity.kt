41e214d6fe71996b9348c3533e8cd8252d44f8d4
msnodeve <msnodeve@gmail.com>
2019-08-02 16:27:10 +0900

    Action bar 제거, UI 조절

--- app/src/main/java/com/seok/gitfordeveloper/MainActivity.kt
+++ app/src/main/java/com/seok/gitfordeveloper/MainActivity.kt
@@ -14,6 +14,7 @@ import com.seok.gitfordeveloper.retrofit.models.UserInfoService
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jsoup.Jsoup
+import java.lang.IndexOutOfBoundsException
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +25,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         cbList = ArrayList()
+
+        doAsync {
+            val doc = Jsoup.connect("https://github.com/msnodeve").get()
+            val regex =
+                """data-count=\"\d+\" data-date=\"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))\"""".toRegex()
+            var i = 1
+            try {
+                while (doc.select("g").select("rect")[i] != null) {
+                    val gTag = doc.select("g").select("rect")[i]
+                    val matchResult: MatchResult? = regex.find(gTag.toString())
+                    var value = matchResult!!.value.replace("\"", "")
+                    var split: List<String> = value.split(" ")
+                    var result1: List<String> = split[0].split("=")
+                    var result2: List<String> = split[1].split("=")
+                    cbList.add(ContributeData(result2[1], result1[1].toInt()))
+                    Log.d("dLog-main-doAsync", result1[1] + " : " + result2[1])
+                    i++
+                }
+            }catch (e : IndexOutOfBoundsException){
+                Log.d("dLog-main-err", e.toString())
+            }
+            setContribute()
+
+        }
 
         ApiUtils.getUserService().getUserInfo("token " + UserInfo.access_token.toString())
             .enqueue(object : retrofit2.Callback<UserInfoService> {
@@ -43,46 +68,43 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             })
-
-        doAsync {
-            val doc = Jsoup.connect("https://github.com/msnodeve").get()
-            val regex =
-                """data-count=\"\d+\" data-date=\"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))\"""".toRegex()
-            var i = 1
-            while (doc.select("g").select("rect")[i] != null) {
-                val gTag = doc.select("g").select("rect")[i]
-                val matchResult: MatchResult? = regex.find(gTag.toString())
-                var value = matchResult!!.value.replace("\"", "")
-                var split: List<String> = value.split(" ")
-                var result1: List<String> = split[0].split("=")
-                var result2: List<String> = split[1].split("=")
-                cbList.add(ContributeData(result2[1], result1[1].toInt()))
-                Log.d("dLog-main-doAsync", result1[1] + " : " + result2[1])
-                i++
-            }
-        }
-        var x = 0
-        var y = 0
-        button.setOnClickListener {
-            for (i in 0..cbList.size - 1) {
-                if (i % 7 == 0 && i != 0) {
-                    x += 30
-                    y = 0
+    }
+    fun setContribute(){
+        runOnUiThread(object : Runnable{
+            override fun run() {
+                var x = 0
+                var y = 0
+                for (i in 0..cbList.size - 1) {
+                    if (i % 7 == 0 && i != 0) {
+                        x += 40
+                        y = 0
+                    }
+                    var topTv1 = TextView(this@MainActivity)
+                    topTv1.layoutParams =
+                        LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT)
+                    topTv1.setTextColor(Color.parseColor("#000000"))
+                    var count = cbList[i].dataCount
+                    if(count ==0){
+                        topTv1.setBackgroundColor(Color.parseColor("#ebedf0"))
+                    }else if(count < 4){
+                        topTv1.setBackgroundColor(Color.parseColor("#c6e48b"))
+                    }else if(count < 7){
+                        topTv1.setBackgroundColor(Color.parseColor("#7bc96f"))
+                    }else if(count < 10){
+                        topTv1.setBackgroundColor(Color.parseColor("#239a3b"))
+                    }else{
+                        topTv1.setBackgroundColor(Color.parseColor("#196127"))
+                    }
+                    topTv1.setTextSize(12f)
+                    topTv1.setPadding(2, 0, 2, 0)
+                    topTv1.setText(cbList[i].dataCount.toString())
+                    topTv1.x = x.toFloat()
+                    topTv1.y = y.toFloat()
+                    dynamicArea.addView(topTv1)
+                    y += 40
                 }
-                var topTv1 = TextView(this)
-                topTv1.layoutParams =
-                    LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT)
-                topTv1.setBackgroundColor(Color.parseColor("#00FFFFFF"))
-                topTv1.setTextColor(Color.parseColor("#000000"))
-                topTv1.setTextSize(11f)
-                topTv1.setPadding(2, 0, 2, 0)
-                topTv1.setText(cbList[i].dataCount.toString())
-                topTv1.x = x.toFloat()
-                topTv1.y = y.toFloat()
-                dynamicArea.addView(topTv1)
-                y += 50
             }
-        }
+        })
     }
 }
 
