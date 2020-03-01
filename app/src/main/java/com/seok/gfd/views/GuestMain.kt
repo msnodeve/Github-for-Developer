4d3d627dbb6d934051fc1c1e1d7adfd7c714c2a6
msnodeve <msnodeve@gmail.com>
2020-03-01 18:44:41 +0900

    study:          - SwipeRefreshLayout 연습 3          - Image, text, 새로고침 완료

--- app/src/main/java/com/seok/gfd/views/GuestMain.kt
+++ app/src/main/java/com/seok/gfd/views/GuestMain.kt
@@ -1,21 +1,25 @@
 package com.seok.gfd.views
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.seok.gfd.R
+import com.seok.gfd.adapter.CustomAdapter
 import kotlinx.android.synthetic.main.activity_guest_main.*
 import java.util.*
 
 class GuestMain : AppCompatActivity() {
-    private val arrayList =ArrayList<String>(listOf("First Element", "Second Element", "Third Element", "Fourth Element", "Fifth Element"))
+    private val personNames =ArrayList(listOf("Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6", "Person 7", "Person 8", "Person 9", "Person 10", "Person 11", "Person 12", "Person 13", "Person 14"))
+    private val personImages = ArrayList(listOf(R.drawable.btn_bottom_help, R.drawable.icon_location, R.drawable.btn_bottom_home, R.drawable.btn_bottom_rank, R.drawable.gfd_app_logo, R.drawable.gfd_launcher, R.drawable.gfd_logo_background, R.drawable.gfd_logo_foreground, R.drawable.github_login_users_background, R.drawable.guest_login_background, R.drawable.ic_launcher_background, R.drawable.icon_bio, R.drawable.rounding_trans_background, R.drawable.rounding_background))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guest_main)
 
-        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, arrayList)
-        listView.adapter = adapter
+        val linearLayoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = linearLayoutManager
+        val customAdapter = CustomAdapter(personNames, personImages, this)
+        recyclerView.adapter = customAdapter
 
         simpleSwipeRefreshLayout.setOnRefreshListener {
             simpleSwipeRefreshLayout.isRefreshing = false
@@ -24,9 +28,10 @@ class GuestMain : AppCompatActivity() {
     }
 
     private fun shuffleItems(){
-        arrayList.shuffle(Random(System.currentTimeMillis()))
-        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, arrayList)
-        listView.adapter = adapter
+        personNames.shuffle(Random(System.currentTimeMillis()))
+        personImages.shuffle(Random(System.currentTimeMillis()))
+        val customAdapter = CustomAdapter(personNames, personImages, this)
+        recyclerView.adapter = customAdapter
     }
 
 }
