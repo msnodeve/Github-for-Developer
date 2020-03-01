b3a411f1a5896aeceb46fa817b0ce55e1fe18ae1
msnodeve <msnodeve@gmail.com>
2020-03-01 18:12:56 +0900

    study:      - SwipeRefreshLayout 연습 2      - Refresh and Shuffled sample string

--- app/src/main/java/com/seok/gfd/views/GuestMain.kt
+++ app/src/main/java/com/seok/gfd/views/GuestMain.kt
@@ -1,26 +1,32 @@
 package com.seok.gfd.views
 
 import android.os.Bundle
-import android.os.Handler
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.seok.gfd.R
 import kotlinx.android.synthetic.main.activity_guest_main.*
 import java.util.*
 
 class GuestMain : AppCompatActivity() {
+    private val arrayList =ArrayList<String>(listOf("First Element", "Second Element", "Third Element", "Fourth Element", "Fifth Element"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guest_main)
 
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, arrayList)
+        listView.adapter = adapter
+
         simpleSwipeRefreshLayout.setOnRefreshListener {
-            Handler().postDelayed({
-                simpleSwipeRefreshLayout.isRefreshing = false
-                val r = Random()
-                val i1 : Int = r.nextInt(100)
-                guestTextView.text = i1.toString()
-            }, 3000)
+            simpleSwipeRefreshLayout.isRefreshing = false
+            shuffleItems()
         }
+    }
+
+    private fun shuffleItems(){
+        arrayList.shuffle(Random(System.currentTimeMillis()))
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, arrayList)
+        listView.adapter = adapter
     }
 
 }
